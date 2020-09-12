1ba2e03816a661d484972afb2b6c46e0276777c7
Caio Poli <caio.poli@ernetwork.com.br>
2020-09-12 00:25:14 -0300

    :tada: Begin a project

---
+++ src/main/java/com/facef/rabbitmqparkinglot/RabbitmqParkinglotApplication.java
@@ -0,0 +1,13 @@
+package com.facef.rabbitmqparkinglot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqParkinglotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqParkinglotApplication.class, args);
+	}
+
+}
