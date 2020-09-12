b02626092c35a7e7e2dfa06713d65edcfd673952
Caio Poli <caio.poli@ernetwork.com.br>
2020-09-12 00:55:29 -0300

    :sparkles: Update RabbitmqParkinglotApplication

--- src/main/java/com/facef/rabbitmqparkinglot/RabbitmqParkinglotApplication.java
+++ src/main/java/com/facef/rabbitmqparkinglot/RabbitmqParkinglotApplication.java
@@ -1,13 +1,27 @@
 package com.facef.rabbitmqparkinglot;
 
+import com.facef.rabbitmqparkinglot.producer.MessageProducer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@Slf4j
 public class RabbitmqParkinglotApplication {
+
+	@Autowired
+	private MessageProducer messageProducer;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RabbitmqParkinglotApplication.class, args);
 	}
 
+	@EventListener(ApplicationReadyEvent.class)
+	public void runningAfterStartup() {
+		log.info("Running method after startup to send messages!");
+		messageProducer.sendFakeMessage();
+	}
 }
